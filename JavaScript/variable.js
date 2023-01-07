f46b0cb7835351e1decb4482501cd67bafc0834d
SanjuManna <sanjumanna2201@gmail.com>
2023-01-07 13:23:14 +0530

    Commit

--- JavaScript/variable.js
+++ JavaScript/variable.js
@@ -43,6 +43,36 @@
 // console.log(PI);
 // console.log(PI*5*5);
 
+// <<<<--------Undefined---------->>>>
+
+// let str;
+// console.log(typeof str); // output ==> undefined
+
+// str = "sanju";
+// console.log(typeof str ); // output ==>  string
+
+// <<<<--------null---------->>>>
+
+// let variable = null;
+// console.log(variable); // output ==> null
+// console.log(typeof variable); // output ==> object
+
+// <<<<--------BigInt---------->>>>
+
+// let num = 54564413616144641444145511411646464; //output ==> 5.456441361614464e+34
+//   num = BigInt(num);
+// num = 125n; // as we add (n) here this is also converted into bigint
+// let num = BigInt(646414964617431464619414464644);
+// console.log(num);
+// console.log(Number.MAX_SAFE_INTEGER);   // 9007199254740991 max value that can be stored as number
+// for number more than .MAX_SAFE_INTEGER we have to use BigInt.
+// we can't add other variable with BigInt
+
+
+
+
+
+
 
 
 
