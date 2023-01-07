45eb2ade0e597537fc1a411674ae61bdd9916a9b
SanjuManna <sanjumanna2201@gmail.com>
2023-01-07 20:19:22 +0530

    Commit

---
+++ JavaScript/operator.js
@@ -0,0 +1,42 @@
+// <<<<--------Comparision Operator---------->>>>
+
+// ==, ===, <=, >=
+// return ture or false
+
+// difference between == vs ===
+// let num1 = "7";
+// let num2 = 7;
+// console.log(num1==num2); // only check the value not data type
+// console.log(num1 === num2); // check value as well as data type
+
+// <<<<--------truthy and falsy value---------->>>>
+
+// falsy values
+// ==>> false , "", null, undefined, 0
+
+// <<<<--------Ternary Operator---------->>>>
+
+// let age = 10;
+// let drink;
+
+// if(age>=5){
+//     drink = "coffee";
+// }
+// else{
+//     drink = "milk";
+// }
+// console.log(age,drink);
+
+// using ternary operator
+// let age = 25;
+// let drink = age>=5 ? "Coffee" : "Milk";
+// console.log(drink);
+
+// <<<<--------Logical Operator(&& ||)---------->>>>
+
+ 
+
+
+
+
+
