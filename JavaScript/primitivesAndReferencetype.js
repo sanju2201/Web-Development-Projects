66788346c358257b10e8f20138c91a6a6147212f
SanjuManna <sanjumanna2201@gmail.com>
2023-01-08 19:11:56 +0530

    Commit

--- JavaScript/primitivesAndReferencetype.js
+++ JavaScript/primitivesAndReferencetype.js
@@ -1,13 +1,36 @@
 // <<<<-------- Primitive and Referance type---------->>>>
 
-let num1 =9; 
-let num2 = num1;  
+// Primitive types
+// let num1 =9; 
+// let num2 = num1;  
 
-console.log(num1); // output ==> 9
-console.log(num2);  // output ==> 9
+// console.log(num1); // output ==> 9
+// console.log(num2);  // output ==> 9
 
-num1++;
-console.log(num1);  // output ==> 10 
-console.log(num2);  // output ==> 9  
+// num1++;
+// console.log(num1);  // output ==> 10 
+// console.log(num2);  // output ==> 9  
+
+// Reference Types (Array)
+
+let arr1 = ["item1", "item2"];
+let arr2 = arr1;
+
+// console.log(arr1);
+// console.log(arr2);  // both the output same
+
+// arr1.push("item3");
+
+// console.log(arr1);
+// console.log(arr2);  // here also both are same
+//Array store references so there is only 1 array in the heap memory and both arr1 and arr2 pointing towards the same array
+
+// To check the both the array are equal or not
+// console.log(arr1 ===arr2);
+
+
+
+
+
 
 
