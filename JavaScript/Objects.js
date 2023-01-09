8893fbb710548674cfa5bf608cba00b8523b090c
SanjuManna <sanjumanna2201@gmail.com>
2023-01-09 12:37:13 +0530

    Commit

---
+++ JavaScript/Objects.js
@@ -0,0 +1,21 @@
+// <<<<--------Objects---------->>>>
+
+// Object are reference type
+// const person = {name:"sanju",age:"24",email:"sanju@manna.com"};
+// // Store as key value pair
+// // Keys are called as property of the objects
+// console.log(person);
+// console.log(typeof person);
+// console.log(person.name);
+// console.log(person.age);
+
+const person = {name:"sanju",
+                age:"24",
+                hobbies : ["cricket","football","Coding"],
+                parents : {mom : "sharmila",
+                           dad : "asit"},
+                email : "sanju@manna",
+                mobile:["8435181981","7000509658"]};
+
+console.log(person);
+console.log(person.parents.dad);
