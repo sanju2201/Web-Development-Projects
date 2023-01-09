ae10c4996917127dd45ae70ed2420f26abfdac5e
SanjuManna <sanjumanna2201@gmail.com>
2023-01-09 23:33:33 +0530

    Commit

--- JavaScript/functionsInJS.js
+++ JavaScript/functionsInJS.js
@@ -1,2 +1,34 @@
 // <<<<--------functions---------->>>>
 
+// Function Declaration
+// function isEvenDeclaration(num){
+//     return num % 2 === 0;
+// }
+// console.log(isEvenDeclaration(4));
+
+// Function Expression
+// const isEvenExpression = function(num){
+//     return num % 2 === 0;
+// }
+// console.log(isEvenExpression(5));
+
+// Arrow Function 
+// Rule to Write a Arrow Functions
+// 1. if there is 0 or more then 1 parameter paranthesis are required. For 1 parameter only its optional.
+// 2. if there is one line of code in literal or body, curly braces are not required.
+// 3. if there is only 1 line of code in body and that also return statement than the return keyword is also optional.
+
+// 1st way
+// const isEvenArrow1 = (num) => {
+//     return num % 2 === 0;
+// };
+// console.log(isEvenArrow1(9));
+
+// 2nd way
+// const isEvenArrow2 = num => num % 2 === 0;
+// console.log(isEvenArrow2(2));
+
+// 3rd way
+// const addTwo = (num1,num2) => num1 + num2;
+// console.log(addTwo(9,5));
+
